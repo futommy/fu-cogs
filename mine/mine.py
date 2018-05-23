2838ca1ccf44dcd8445883fec734b388da2baae9
TOM <38869405+futommy@users.noreply.github.com>
2018-05-23 15:09:26 -0700

    Update mine.py

--- mine/mine.py
+++ mine/mine.py
@@ -15,7 +15,7 @@ class MinerCog:
 		self.settings = dataIO.load_json("data/tom/minesettings.json")
 		if not 'settings' in self.settings:
 			self.settings = {'settings': {}}
-		self.debugroom = self.bot.get_channel('443633597417521163')
+		self.debugroom = self.bot.get_channel('425980791550377996')
 		self.bank = self.bot.get_cog('Economy').bank
 		if not 'mineamount' in self.settings['settings']:
 			self.settings['settings']['mineamount'] = 20
